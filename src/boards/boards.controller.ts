7d74c675fac7ae2fffc1cf96d20920e84c87949b
hyeririjeon <jeonhyeri5555@gmail.com>
2025-07-01 20:28:02 +0900

    feat: 게시물 생성 DTO 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 /**
  * nest g controller boards --no-spec
@@ -18,9 +19,8 @@ export class BoardsController {
 
     @Post()
     createBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
+        @Body() createBoardDto: CreateBoardDto
     ): Board {
-        return this.boardsService.createBoard(title, description);
+        return this.boardsService.createBoard(createBoardDto);
     }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 /**
  * nest g service boards --no-spec
@@ -17,7 +18,9 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string) {
+    createBoard(createBoardDto: CreateBoardDto) {
+        const { title, description } = createBoardDto;
+        
         const board: Board = {
             id: uuid(),
             title,
